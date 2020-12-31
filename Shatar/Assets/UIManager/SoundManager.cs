39a53a145cd0fb3124c464824cea2c5820cf02f1
LauraaHR <67450547+LauraaHR@users.noreply.github.com>
2020-12-31 13:29:46 +0100

    Sound Testing
    Pequeñas pruebas con sonido. No afecta mucho a la copia anterior

---
+++ Shatar/Assets/UIManager/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //Menu music
+    [SerializeField] AudioSource menuMusic;
+
+    //Sound effects
+    [SerializeField] AudioSource buttonSound;
+}

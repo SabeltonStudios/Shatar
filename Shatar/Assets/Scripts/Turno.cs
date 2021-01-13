b4b2b8e566b12482d0484d8999a708cc351a4a13
rociiocs <49315467+rociiocs@users.noreply.github.com>
2021-01-13 12:47:51 +0100

    DeshacerMovimientoCambiadoPila
    Deshacer movimiento cambiado a realizar con una pila para gestionar los errores que daba anteriormente.

---
+++ Shatar/Assets/Scripts/Turno.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turno : MonoBehaviour
+{
+    public Node previousNode;
+    public TipoPieza previousPieza;
+    public GameObject enemieEat;
+    public int vallaHorseID;
+    public int vallaCastleID;
+    public bool cambioPieza;
+
+    public Turno(Node p,TipoPieza tp,GameObject et, int vh, int vc, bool cambioP)
+    {
+        previousNode = p;
+        previousPieza = tp;
+        enemieEat = et;
+        vallaHorseID = vh;
+        vallaCastleID = vc;
+        cambioPieza = cambioP;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
